79b52eb1fb782b26f0e526727debe69f454e712d
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2021-12-18 23:47:57 +0530

    Day 4 - Part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -55,6 +55,63 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        var nums = input[0].split(",").map { it.toInt() }
+        var mat = mutableListOf<MutableList<MutableList<Int>>>()
+        var row = mutableListOf<MutableList<Int>>()
+        var column = mutableListOf<MutableList<Int>>()
+        var count = 0
+        for(i in 1 until input.size){
+            if(input[i].isEmpty()){
+                mat.add(mutableListOf())
+                count++
+            }else{
+                var x =  input[i].trim()
+                var item = x.split("\\s+".toRegex()).map (String::toInt)
+                var new = item as MutableList<Int>
+                mat[count-1].add(new)
+                row.add(mutableListOf())
+                row[count-1].add(5)
+                column.add(mutableListOf())
+                column[count-1].add(5)
+            }
+        }
+        var doneCount = mat.size
+        var matC = IntArray(mat.size)
+        for(i in 0 until nums.size){
+            for(j in 0 until mat.size){
+                if(matC[j] == -1) continue
+                for(r in 0..4){
+                    for(c in 0..4){
+                        if(mat[j][r][c] == nums[i]){
+                            row[j][r]--
+                            column[j][c]--
+                            var m = mat[j][r][c]
+                            mat[j][r][c] = -1
+                            if(row[j][r] == 0 || column[j][c] == 0){
+                                matC[j] = - 1
+                                doneCount--
+                                if(doneCount == 0){
+                                    var ans = 0
+                                    for(nr in 0..4){
+                                        for(nc in 0..4){
+                                            if(mat[j][nr][nc] != -1){
+                                                ans += mat[j][nr][nc]
+                                            }
+                                        }
+                                    }
+                                    ans *= m
+                                    return ans
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
         return input.size
     }
 
